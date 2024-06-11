37d8200a799e1dc7425e0d098ea7e5ef82203998
WSI <websparkinnovations@gmail.com>
2024-06-11 12:48:22 -0400

    edited env variable

--- lib/social.ts
+++ lib/social.ts
@@ -1,1 +1,1 @@
-export const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_TOKEN}`;
+export const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink&access_token=${process.env.INSTAGRAM_TOKEN}`;
