f3f6f2d81113eaa9c6a680990f604fa70ac45bb0
enesctnn <enespcetin@gmail.com>
2025-03-08 23:11:44 +0300

    refactor: refactored images add banner images to public folder. - conver png images to webp

--- lib/home-slide-images.ts
+++ lib/home-slide-images.ts
@@ -1,18 +1,14 @@
-export const images = [
-  {
-    src: 'https://cdn.discordapp.com/attachments/775573349983715328/1248656037905432596/e776edf6-e791-44c5-8f1c-9bfe73826c67.png?ex=6669bb0e&is=6668698e&hm=ecb67e8ae0eacda93c735012a0514f2468b9bb6e6526ffa6904ca1ccce00c9f5&',
-    alt: 'ilic white, red, yellow drinks',
-  },
-  {
-    src: 'https://cdn.discordapp.com/attachments/775573349983715328/1249928739156918333/36512503_1023792077777959_1926782875139571712_n.png?ex=66691659&is=6667c4d9&hm=2635c373e3ec06bae5c02d2ee426d631f17cc8a58480d9b3c881e109bb2adfae&',
-    alt: 'vanilla rolled ice cream with choco syrup',
-  },
-  {
-    src: 'https://cdn.discordapp.com/attachments/775573349983715328/1250227597393006684/water_mark_removed_banner_img.jpeg?ex=666ec9ef&is=666d786f&hm=a316a127c43da2b010fc0109d6ff844ec2164393ca5313901de3f552d5d85ce2&',
-    alt: '4 ice cream row',
-  },
-  {
-    src: 'https://cdn.discordapp.com/attachments/775573349983715328/1250234628032303165/banner_image_2.jpeg?ex=666ed07b&is=666d7efb&hm=16dcec1012a6baa67b579efcbbf79296a8647453fe0885b06967f68e698d43ac&',
-    alt: '4 drinks row',
-  },
+export const images: Record<'src' | 'alt', string>[] = [
+	{
+		src: '/images/drinks.webp',
+		alt: 'milk shakes'
+	},
+	{
+		src: '/images/ice_cream_cups.webp',
+		alt: 'vanilla rolled ice cream with choco syrup'
+	},
+	{
+		src: '/images/icecream_rulos.webp',
+		alt: 'ice cream rolls'
+	}
 ];
